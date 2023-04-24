afcd49f0b6ce98a51a389403631e3200a80257ad
kshoon <kshoon0518@gmail.com>
2023-04-24 20:35:51 +0900

    feat: 초기 세팅 및 db연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { BookmarksModule } from './bookmarks/bookmarks.module';
+import { AnalysisModule } from './analysis/analysis.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardsModule, UserModule, AuthModule, UsersModule, PostsModule, CommentsModule, BookmarksModule, AnalysisModule],
 })
 export class AppModule {}
