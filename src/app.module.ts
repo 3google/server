17c3e1308c14a84be9dfe5324e70a469ac81cec3
kshoon <kshoon0518@gmail.com>
2023-04-26 18:21:30 +0900

    feat: 파일구조 정리, 스키마 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
-import { BoardsModule } from './boards/boards.module';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { CommentsModule } from './comments/comments.module';
 import { BookmarksModule } from './bookmarks/bookmarks.module';
 import { AnalysisModule } from './analysis/analysis.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [BoardsModule, UserModule, AuthModule, UsersModule, PostsModule, CommentsModule, BookmarksModule, AnalysisModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    PostsModule,
+    CommentsModule,
+    BookmarksModule,
+    AnalysisModule,
+  ],
 })
 export class AppModule {}
