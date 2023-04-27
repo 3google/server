30371997af32f652e1808493cce4fde56c848651
kshoon <kshoon0518@gmail.com>
2023-04-27 10:36:49 +0900

    feat: postAPI 작성

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,8 +1,8 @@
 import { Emotion, BoardType } from '@prisma/client';
 export class CreatePostDto {
+  author_id: number; //나중에 토큰에서 읽어오기
   title: string;
   emotion: Emotion;
   content: string;
-  author_id: number;
   board_type: BoardType;
 }
